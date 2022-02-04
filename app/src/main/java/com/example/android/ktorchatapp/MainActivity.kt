e66fad3e60c78ddabff1a85a498dfcdeefbfe619
Jøran Haines <joran.haines@gmail.com>
2022-02-04 17:26:16 +0530

    Adds chat screen

--- app/src/main/java/com/example/android/ktorchatapp/MainActivity.kt
+++ app/src/main/java/com/example/android/ktorchatapp/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.android.ktorchatapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.android.ktorchatapp.presentation.chat.ChatScreen
+import com.example.android.ktorchatapp.presentation.username.UsernameScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            val navController = rememberNavController()
+            NavHost(navController = navController, startDestination = "username_screen") {
+                composable("username_screen") {
+                    UsernameScreen(onNavigate = navController::navigate)
+                }
+                composable(
+                    route = "chat_screen/{username}",
+                    arguments = listOf(
+                        navArgument(name = "username") {
+                            type = NavType.StringType
+                            nullable = true
+                        }
+                    )
+                )
+                {
+                    val username = it.arguments?.getString("username")
+                    ChatScreen(username = username)
+                }
+            }
+        }
     }
 }
